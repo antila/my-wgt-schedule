73a65bc202601f0152c796c4e09f218a168aade9
Anders Antila <anders.antila@chas.se>
2024-05-14 00:58:27 +0200

    fix: loading of data

--- app/src/hooks/dataHook.ts
+++ app/src/hooks/dataHook.ts
@@ -1,21 +1,21 @@
+import { readFileSync } from 'node:fs'
 import type { Data } from '@/types/data'
 import type { DiscogsData } from '@/types/discogs'
 import type { VenueList } from '@/types/venue'
 import { useQuery } from '@tanstack/react-query'
+import * as dataDiscogs from '../../public/data-discogs.json'
+import * as data from '../../public/data.json'
 
 export const getData = async (): Promise<Data> => {
-  const data = await fetch('https://wgt.zauber.tech/data.json', { cache: 'no-store' })
-  return await data.json()
-}
-
-export const getClientData = async (): Promise<Data> => {
-  const data = await fetch('/data.json', { cache: 'no-store' })
-  return await data.json()
+  return JSON.parse(JSON.stringify(data))
+  // const data = await fetch('https://wgt.zauber.tech/data.json', { cache: 'no-store' })
+  // return await data.json()
 }
 
 export const getDiscogsData = async (): Promise<DiscogsData[]> => {
-  const data = await fetch('https://wgt.zauber.tech/data-discogs.json', { cache: 'no-store' })
-  return await data.json()
+  return JSON.parse(JSON.stringify(dataDiscogs))
+  // const data = await fetch('https://wgt.zauber.tech/data-discogs.json', { cache: 'no-store' })
+  // return await data.json()
 }
 
 export const getVenues = async (): Promise<VenueList[]> => {
